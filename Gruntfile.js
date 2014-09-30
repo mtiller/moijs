1da358c5578ef6501b29f43017099a0b520142fe
Michael Tiller <michael.tiller@gmail.com>
2014-09-30 15:21:51 -0400

    Grunt now builds something, but I don't think it is correct

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,18 @@
 'use strict';
 
 module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-jison');
+
 	grunt.initConfig({
 		jison: {
 			target : {
 				options: { moduleType: 'amd' },
 				files: {
-					'molexer-amd.js': ['molexer.jison',  'molexer.jisonlex'}
+					'molexer-amd.js': ['molexer.jison',  'molexer.jisonlex']
 				}
 			}
 		}
 	});
-}
 
+	grunt.registerTask('default', ['jison']);
+}
